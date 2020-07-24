137277fa3485193a8db119ca1a31dcdb2ff2eb86
volo <gunko63@gmail.com>
2020-07-24 17:46:15 -0400

    3 tests done

--- e2e_AngularTest/dist/out-tsc/homeSpec.js
+++ e2e_AngularTest/dist/out-tsc/homeSpec.js
@@ -2,12 +2,41 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var protractor_1 = require("protractor");
 describe("Sample test suit for Country App", function () {
-    it("Default vallue should be displayed", function () {
+    beforeEach(function () {
         protractor_1.browser.get('file:///home/volo/Desktop/Protractor_Test/e2e_AngularTest/application.html');
+    });
+    it("Default vallue should be displayed", function () {
         var sampleElement = protractor_1.element(protractor_1.by.id('country')); //ElementFinder function used here 
         sampleElement.getAttribute('innerText').then(function (text) {
             console.log(text);
+            expect(text).toBe('.....');
         });
+        protractor_1.browser.sleep(2000);
+    });
+    it("Default Input should match with displayed field", function () {
+        var tring = protractor_1.element(protractor_1.by.id('try'));
+        tring.click();
+        var abc = protractor_1.browser.switchTo().alert();
+        abc.accept();
+        var sampleElement = protractor_1.element(protractor_1.by.id('country'));
+        sampleElement.getAttribute('innerText').then(function (text) {
+            console.log(text);
+            expect(text).toBe('usa');
+        });
+        protractor_1.browser.sleep(2000);
+    });
+    it("Input code should match with displayed field", function () {
+        var tring = protractor_1.element(protractor_1.by.id('try'));
+        tring.click();
+        var abc = protractor_1.browser.switchTo().alert();
+        abc.sendKeys('372');
+        abc.accept();
+        var sampleElement = protractor_1.element(protractor_1.by.id('country'));
+        sampleElement.getAttribute('innerText').then(function (text) {
+            console.log(text);
+            expect(text).toBe('372');
+        });
+        protractor_1.browser.sleep(2000);
     });
 });
 //# sourceMappingURL=../../src/dist/out-tsc/homeSpec.js.map
--- e2e_AngularTest/src/homeSpec.ts
+++ e2e_AngularTest/src/homeSpec.ts
@@ -1,14 +1,51 @@
 
 import { browser, element, by, ElementFinder } from "protractor";
+import { Console } from "console";
+import {Alert} from 'selenium-webdriver';
+
+
 
 describe("Sample test suit for Country App", () => {
+
+  beforeEach(()=>{
+    browser.get('file:///home/volo/Desktop/Protractor_Test/e2e_AngularTest/application.html');
+  });
+
     it("Default vallue should be displayed", ()=> {
-        browser.get('file:///home/volo/Desktop/Protractor_Test/e2e_AngularTest/application.html');
         let sampleElement: ElementFinder = element(by.id('country')); //ElementFinder function used here 
         sampleElement.getAttribute('innerText').then((text) => {
           console.log(text);
           expect(text).toBe('.....');
         });
-        
+        browser.sleep(2000);
     });
+    it("Default Input should match with displayed field", () =>  {
+        let tring:ElementFinder = element(by.id('try'));
+        tring.click();
+
+        let abc = browser.switchTo().alert();
+        abc.accept();
+
+        let sampleElement: ElementFinder = element(by.id('country')); 
+        sampleElement.getAttribute('innerText').then((text) => {
+          console.log(text);
+          expect(text).toBe('usa');
+        });
+        browser.sleep(2000);
+    })
+    it("Input code should match with displayed field", () =>{
+      let tring:ElementFinder = element(by.id('try'));
+        tring.click();
+
+        let abc = browser.switchTo().alert();
+        abc.sendKeys('372');
+        abc.accept();
+
+        let sampleElement: ElementFinder = element(by.id('country')); 
+        sampleElement.getAttribute('innerText').then((text) => {
+          console.log(text);
+          expect(text).toBe('372');
+        });
+        browser.sleep(2000);
+    })
 });
